38b2b35571025dc24b6d7462ab24e6bf2bd0eec5
Lord Bony <xbony22@gmail.com>
2014-06-29 10:30:25 -0400

    Added scala version (don't know what I'm doing there though :p)

---
+++ TicTacScalaVersion/src/TicTac.scala
@@ -0,0 +1,8 @@
+object TicTac {
+
+  //I don't really know what I'm doing
+  def main(){
+    println("Startup!")
+  }
+  
+}
---
+++ TicTacScalaVersion/src/square.scala
@@ -0,0 +1,19 @@
+object square {
+
+  /** 
+	 * 0 represents white
+	 * 1 represents red
+	 * 2 represents blue
+	 * 
+	 * */
+  private val color = 0
+ 
+  /** 
+	 * Returns the color as an integer
+	 * 
+	 * @author xbony2
+	 * */
+  def getColor(): Int = {
+		return color;
+	}
+}
